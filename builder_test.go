56c15d7c5e2ab4c62833c59748ef6a35a426d876
Surya Herdiyanto <suryaherdiyanto@gmail.com>
2025-02-16 11:33:28 +0800

    Feat: improve where combination

--- builder_test.go
+++ builder_test.go
@@ -34,3 +34,19 @@ func TestWithWhere(t *testing.T) {
 		t.Errorf("Unexpected SQL result, got: %s", builder.GetSql())
 	}
 }
+
+func TestWithMultipleWhere(t *testing.T) {
+	db, _ := sql.Open("sqlite3", ":memory:")
+
+	builder := NewSQLBuilder("sqlite", db)
+
+	builder.
+		Select([]string{"*"}).
+		Table("users").
+		Where("email", "=", "johndoe@gmail.com").
+		Where("access_role", "<", 3)
+
+	if builder.GetSql() != "SELECT * FROM users WHERE email = 'johndoe@gmail.com' AND access_role < 3" {
+		t.Errorf("Unexpected SQL result, got: %s", builder.GetSql())
+	}
+}
