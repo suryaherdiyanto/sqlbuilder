b7b328a8a214743be68c58f09657be259100f01d
Surya Herdiyanto <suryaherdiyanto@gmail.com>
2025-02-16 14:17:54 +0800

    Feat: WhereIn logic

--- builder.go
+++ builder.go
@@ -25,7 +25,7 @@ type Builder interface {
 	Table(table string) Builder
 	Select(cols []string) Builder
 	Where(column string, op string, val interface{}) Builder
-	WhereIn(column string, d []string) Builder
+	WhereIn(column string, d interface{}) Builder
 	GetSql() string
 }
 
@@ -60,13 +60,39 @@ func (b *SQLBuilder) Where(column string, op string, val interface{}) Builder {
 	return b
 }
 
-func (b *SQLBuilder) WhereIn(column string, d []string) Builder {
+func (b *SQLBuilder) WhereIn(column string, values interface{}) Builder {
 	clause := "WHERE"
+	var inValues string
+
 	if hasWhere(b.Statement) {
 		clause = "AND"
 	}
 
-	b.Statement += fmt.Sprintf(" %s %s in(%s)", clause, column, strings.Join(d, ","))
+	ref := reflect.ValueOf(values)
+	if ref.Kind() != reflect.Slice {
+		return b
+	}
+	lenValues := ref.Len()
+
+	for i := 0; i < lenValues; i++ {
+		vRef := ref.Index(i)
+		val := ref.Index(i).Interface()
+
+		con := fmt.Sprintf("%v", val)
+
+		if vRef.Kind() == reflect.String {
+			con = fmt.Sprintf("'%s'", val)
+		}
+
+		if i < (lenValues - 1) {
+			con += ", "
+		}
+
+		inValues += con
+	}
+
+
+	b.Statement += fmt.Sprintf(" %s %s IN(%s)", clause, column, inValues)
 	return b
 }
 
