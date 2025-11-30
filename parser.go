4e0ba54f6fd1d573c1fea883527ef5a7b5bde173
suryaherdiyanto <suryaherdiyanto@gmail.com>
2025-11-30 15:33:57 +0800

    add parser interfaces for WHERE, JOIN, and ORDER clauses

---
+++ parser.go
@@ -0,0 +1,37 @@
+package sqlbuilder
+
+type WhereParser interface {
+	Parse() string
+}
+
+type WhereInParser interface {
+	Parse() string
+}
+
+type WhereBetweenParser interface {
+	Parse() string
+}
+
+type WhereNotBetweenParser interface {
+	Parse() string
+}
+
+type JoinParser interface {
+	Parse() string
+}
+
+type StatementParser interface {
+	Parse() string
+}
+
+type OrderParser interface {
+	Parse() string
+}
+
+type ClauseParser interface {
+	ParseJoins() string
+	ParseWhereNotBetweens() string
+	ParseWhereIn() string
+	ParseWhereNotIn() string
+	ParseOrdering() string
+}
