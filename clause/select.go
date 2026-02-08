52a909f53d27d69879fdc8ae1de68c6ef0ebb41c
suryaherdiyanto <suryaherdiyanto@gmail.com>
2026-02-08 10:22:17 +0800

    append arguments from wheres

--- clause/select.go
+++ clause/select.go
@@ -9,8 +9,14 @@ type Select struct {
 	Limit   Limit
 	Offset  Offset
 	WhereStatements
+	Values []any
 }
 
-func (s Select) Parse(dialect SQLDialector) string {
+func (s Select) Parse(dialect SQLDialector) (string, Select) {
 	return dialect.ParseSelect(s)
+}
+
+func (s Select) GetArguments() []any {
+	s.Values = append(s.Values, s.WhereStatements.Values...)
+	return s.Values
 }
