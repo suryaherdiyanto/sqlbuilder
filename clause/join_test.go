a547d3a426efd6f3517d8679133f3851f9a48c66
suryaherdiyanto <suryaherdiyanto@gmail.com>
2026-03-15 16:37:13 +0800

    rework join structure

--- clause/join_test.go
+++ clause/join_test.go
@@ -10,12 +10,11 @@ func TestJoinClauseParsing(t *testing.T) {
 	dialect := dialect.New("?", "`", "`")
 	join := Join{
 		Type:        LeftJoin,
-		FirstTable:  "users",
 		SecondTable: "orders",
 		On: JoinON{
 			Operator:   OperatorEqual,
-			LeftField:  "id",
-			RightField: "user_id",
+			LeftField:  "users.id",
+			RightField: "orders.user_id",
 		},
 	}
 
@@ -31,12 +30,11 @@ func TestJoinClauseParsingPG(t *testing.T) {
 	dialect := dialect.NewPostgres()
 	join := Join{
 		Type:        LeftJoin,
-		FirstTable:  "users",
 		SecondTable: "orders",
 		On: JoinON{
 			Operator:   OperatorEqual,
-			LeftField:  "id",
-			RightField: "user_id",
+			LeftField:  "users.id",
+			RightField: "orders.user_id",
 		},
 	}
 
@@ -56,12 +54,11 @@ func TestStatementWithJoin(t *testing.T) {
 		Joins: []Join{
 			{
 				Type:        InnerJoin,
-				FirstTable:  "users",
 				SecondTable: "orders",
 				On: JoinON{
-					LeftField:  "id",
+					LeftField:  "users.id",
 					Operator:   OperatorEqual,
-					RightField: "user_id",
+					RightField: "orders.user_id",
 				},
 			},
 		},
@@ -86,6 +83,52 @@ func TestStatementWithJoin(t *testing.T) {
 	}
 }
 
+func TestStatementMultipleJoin(t *testing.T) {
+	dialect := dialect.New("?", "`", "`")
+	statement := Select{
+		Table:   "users",
+		Columns: []string{"users.*", "user_orders.total", "products.name"},
+		Joins: []Join{
+			{
+				Type:        InnerJoin,
+				SecondTable: "user_orders",
+				On: JoinON{
+					LeftField:  "users.id",
+					Operator:   OperatorEqual,
+					RightField: "user_orders.user_id",
+				},
+			},
+			{
+				Type:        InnerJoin,
+				SecondTable: "products",
+				On: JoinON{
+					LeftField:  "user_orders.product_id",
+					Operator:   OperatorEqual,
+					RightField: "products.id",
+				},
+			},
+		},
+	}
+	where := WhereStatements{
+		Where: []Where{
+			{
+				Field: "user_orders.total",
+				Op:    OperatorGreaterThan,
+				Value: 10000,
+			},
+		},
+	}
+
+	stmt, _ := statement.Parse(dialect)
+	stmt += where.Parse(dialect)
+
+	expected := "SELECT `users`.*,`user_orders`.`total`,`products`.`name` FROM `users` INNER JOIN `user_orders` ON `users`.`id` = `user_orders`.`user_id` INNER JOIN `products` ON `user_orders`.`product_id` = `products`.`id` WHERE `user_orders`.`total` > ?"
+
+	if stmt != expected {
+		t.Errorf("Expected: %s, but got: %s", expected, stmt)
+	}
+}
+
 func TestStatementWithJoinPG(t *testing.T) {
 	dialect := dialect.NewPostgres()
 	statement := Select{
@@ -94,12 +137,11 @@ func TestStatementWithJoinPG(t *testing.T) {
 		Joins: []Join{
 			{
 				Type:        InnerJoin,
-				FirstTable:  "users",
 				SecondTable: "orders",
 				On: JoinON{
-					LeftField:  "id",
+					LeftField:  "users.id",
 					Operator:   OperatorEqual,
-					RightField: "user_id",
+					RightField: "orders.user_id",
 				},
 			},
 		},
